3e25e1c616812a873833117f98460b87ea46abd5
karoid <shj5508@naver.com>
2023-01-30 09:27:29 +0900

    [Feat] Friendly Name added

---
+++ data/FriendlyName/ko_kr.js
@@ -0,0 +1,43 @@
+export default [
+	['AttackChampion', '공격:챔프'],
+	['AttackGeneral', '공격:기본'],
+	['BasicAttack', '공격'],
+
+	['Death', '죽음'],
+
+	['JokeResponse', '대사:농담'],
+	['TauntResponse', '대사:조롱'],
+	['Response', '응답'],
+	['Joke', '농담'],
+	['Taunt', '조롱'],
+	['Laugh', '웃음'],
+	['Dance', '춤'],
+
+	['FirstEncounterGeneral', '첫만남:기본'],
+	['FirstEncounter', '첫만남'],
+	['MoveFirst', '이동:첫이동'],
+	['MoveLong', '이동:장거리'],
+	['MoveStandard', '이동'],
+	['FirstMove', '이동:첫이동'],
+	['LongMove', '이동:장거리'],
+	['MoveOrderLong', '이동:장거리'],
+	['MoveOrder', '이동'],
+	['LevelUp', '레벨업'],
+	['ShopOpen', '상점'],
+
+
+	['Ally', '동맹'],
+	['Enemy', '적'],
+
+	['Champion', '챔프'],
+	['Kill', '킬'],
+	['Nearby', '근처'],
+	['Move', '이동'],
+	['Attack', '공격'],
+	['Recall', '귀환'],
+	['Receive', '도달'],
+
+	['Female', '여성'],
+	['Male', '남성'],
+
+];
